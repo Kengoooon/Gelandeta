c18c486e020dfd7d1c7af56088973d3d7f18cf54
藤本健悟 <kengoooon@Macbook-Air.local>
2017-02-10 23:53:35 +0900

    複数拠点プロット

--- GelanData/ViewController.swift
+++ GelanData/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import GoogleMaps
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, GMSMapViewDelegate{
     
     //ゲレンデ配列
     var csvGelandeArray:[String] = []
@@ -22,27 +22,43 @@ class ViewController: UIViewController {
         //CSVファイルからゲレンデデータを読み込み
         let loadFile = LoadFile()
         csvGelandeArray = loadFile.loadCSV("gelande")
-        print(csvGelandeArray)
         
-        //とりあえず苗場を格納
-        gelandeArray = csvGelandeArray[areaCount].components(separatedBy: ",")
-        print(gelandeArray)
         // とりあえず地図を表示
-        let camera = GMSCameraPosition.camera(withLatitude: 35.67,longitude: 139.74, zoom: 8)
-        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        var camera = GMSCameraPosition.camera(withLatitude: 35.67,longitude: 139.74, zoom: 8)
+        var mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
         mapView.isMyLocationEnabled = true
+        mapView.delegate = self
         self.view = mapView
         
-        let marker = GMSMarker()
-        let marker2 = GMSMarker()
-        marker.position = CLLocationCoordinate2DMake(35.67, 139.74)
-        marker2.position = CLLocationCoordinate2DMake(Double(gelandeArray[3])!, Double(gelandeArray[4])!)
-        //marker.title = "SannoParkTower"
-        //marker.snippet = "Japan"
-        marker.map = mapView
-        marker2.map = mapView
+        
+        //ゲレンデデータの読み込み関数
+        func nextGerande(){
+            gelandeArray.removeAll()
+            gelandeArray = csvGelandeArray[areaCount].components(separatedBy: ",")
+            let marker = GMSMarker()
+            marker.position = CLLocationCoordinate2DMake(Double(gelandeArray[3])!, Double(gelandeArray[4])!)
+            marker.title = String(gelandeArray[1])!
+            marker.snippet = String(gelandeArray[5])!
+            marker.map = mapView
+            areaCount += 1
+        }
+        
+        
+        for _ in 0..<csvGelandeArray.count - 1{
+            nextGerande()
+        }
+        
+        
     }
-
+    
+    func mapView(mapView: GMSMapView, didTapMarker marker: GMSMarker){
+        // ユーザーに関わる処理
+        print("タップイベント")
+    }
+    func mapView(_ mapView: GMSMapView, didTapAt coordinate: CLLocationCoordinate2D) {
+        print("You tapped at \(coordinate.latitude), \(coordinate.longitude)")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
